a1555153c4d5fb1b452c988f068da2855108424a
Henry Nyobange <henrynyobange@gmail.com>
2023-07-13 06:01:20 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""Module: 9-student
-This is a class Student that defines a student by:
-- First_name
-- Last_name
-- Age
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """Class that defines a student.
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json().
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """It accept and assign value to the argument of student class
+        """Initialize a new Student.
 
         Args:
-            - first_name
-            - last_name
-            - age
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
-    def to_json(self):
-        """It retrieves a dictionary representation of a Student instance
 
-        Args:
-            - self
-            Return a dictionary of a JSON object
-        """
-        
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
         return self.__dict__
