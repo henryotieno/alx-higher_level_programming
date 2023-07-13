abced4aed7f41810e5abb6ccf30d3bb7def20eca
Henry Nyobange <henrynyobange@gmail.com>
2023-07-13 05:59:44 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-"""Module: 8-class_to_json
-This is a function that returns the dictionary description 
-This is return with a simple data structure (list, dictionary, string, integer and boolean) for JSON
-"""
+"""Defines a Python class-to-JSON function."""
 
 
 def class_to_json(obj):
-    """Return the sample data structure
-    As list, dictionary, string, integer and boolean
-    As JSON
-
-    Args:
-        - obj: The object to be open
-    """
-
+    """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
