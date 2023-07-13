edbf448f463d9b3a135311bbf40e300d5f122833
Henry Nyobange <henrynyobange@gmail.com>
2023-07-13 05:52:03 +0300

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""Module: 4-from_json_string
-Returns an object (Python data structure) represented by a JSON string
-"""
-
-
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
 import json
 
 
 def from_json_string(my_str):
-    """This is a function that returns an object represented by a JSON string
-    Returns (Python data structure)
-
-    Args:
-        - my_str: the argument to be deserialized
-    """
-    
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
