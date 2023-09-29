ddbc10a457fc7584d07d57761432676f8c5ab629
Henry Nyobange <henrynyobange@gmail.com>
2023-09-30 00:21:16 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status
-"""
-
+""" fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as req
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        data = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
